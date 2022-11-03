870b1dff08e64cba1258af970ef0d98729b504d7
AndrewDority <adority121@gmail.com>
2022-11-02 19:42:56 -0500

    created font object in MainMenu.__init__()

--- states/main_menu.py
+++ states/main_menu.py
@@ -4,11 +4,13 @@
     state is the application's entry state. MainMenu inherits from menu which inherits from state.
 """
 
+import pygame
 from states.menu import Menu
 
 class MainMenu(Menu):
     def __init__(self) -> None:
         super().__init__()
+        self.font = pygame.font.Font(None) # 'None' uses the default pygame font: freesansbold
 
     def update(self) -> None:
         super().update()
