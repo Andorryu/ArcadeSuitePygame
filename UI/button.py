6fd56e82b12cc6945a139b05115dd9444666d5ae
AndrewDority <adority121@gmail.com>
2022-11-06 18:26:52 -0600

    refactored button.py and menu.py and added UI_layer.py and custom_text.py

--- UI/button.py
+++ UI/button.py
@@ -1,60 +1,31 @@
 """
     button.py
-    This class holds data related to button functionality and how they should be displayed,
-    and functions that statically change the button in some way
-    button.py and menu.py modules are fairly tightly coupled
+    This class holds data related to button functionality and how they should be displayed
 """
 
 import pygame
 import color
 
 class Button:
-    # IMPORTANT: NEED TO CHANGE SIZE PARAM TO PADDING SINCE BUTTON SIZE SHOULD FORM AROUND TEXT
-    def __init__(self, pos: tuple[int, int], size: tuple[int, int], text: str, font_size: int, submit_handler,
-            primary_color=color.WHITE, secondary_color=color.BLACK, active=True, selected=False
-        ) -> None:
+    """
+        DESC:
+            Button consists of a rectangle background with text centered inside. The 
+            primary and secondary colors are swapped when the button is selected.
+        PARAMS:
+            * text - string of text centered in button.
+            * pos - double of ints, refers to the center of the button.
+            * font_size - size of the text in the button.
+            * primary_color - triple of ints (R, G, B), describes the text color when 
+            the button is not selected and the background color when the button is selected.
+            * secondary_color - triple of ints (R, G, B), describes the background color 
+            when the button is not selected and the text color when the button is selected.
+    """
+    def __init__(self, text: str, pos: tuple[int, int], font_size: int, primary_color: tuple[int, int, int],
+        secondary_color: tuple[int, int, int], padding: tuple[int, int], active=True, selected=False, submitted=False):
+        # set up button in its initial state
+        pass
 
-        pygame.font.init() # init font module
+    # button states are updated in UILayer since they depend on other buttons
 
-        self.pos = pos
-        self.text = text
-        # SUBMIT HANDLER
-        self.submit_handler = submit_handler # function to be called when button is submitted
-        # END SUBMIT HANDLER
-
-        # OPT PARAMS
-        self.active = active
-        self.selected = selected
-        # COLOR PARAMS
-        # these change in Menu
-        self.background_color = secondary_color
-        self.font_color = primary_color
-        # END COLOR PARAMS
-        # END OPT PARAMS
-
-        # PYGAME PARAMS
-        # create rect that obeys pos and size params
-        self.rect = pygame.Rect(left_top_width_height = (
-            pos[0] - size[0]/2,
-            pos[1] - size[1]/2,
-            size[0],
-            size[1]
-        ))
-        # create font
-        self.font = pygame.font.Font(None, font_size)
-        # create text
-        self.text_surface = self.font.render(text, True, self.font_color)
-        self.text_rect = self.text_surface.get_rect(
-            center=self.pos
-        )
-        # END PYGAME PARAMS
-
-    def set_font_color(self, color) -> None:
-        self.font_color = color
-        self.text = self.font.render(self.text, True, self.font_color)
-        self.text_rect = self.text_surface.get_rect(
-            center=self.pos
-        )
-
-    def set_background_color(self, color) -> None:
-        self.background_color = color
+    def render(self):
+        pass
